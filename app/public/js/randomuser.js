b312454eee0006022916f900ca690f8aa63caf3b
mbublick <54515932+mbublick@users.noreply.github.com>
2019-09-15 16:12:18 -0400

    Adjusted code for app folder

--- app/public/js/randomuser.js
+++ app/public/js/randomuser.js
@@ -8,12 +8,6 @@ var waitingApp = new Vue({
       fetch('https://randomuser.me/api')
       .then(response => response.json())
       .then(json => {waitingApp.user = json.results[0]});
-
-      // Means the same at this
-      // fetch('https://randomuser.me/api/')
-      // .then(function(response) {return response.json()})
-      // .then(function(json) {waitingApp.people = json});
-
     }
   },
   created() {
